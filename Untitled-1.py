57dca8499a77b67b9a64da59dd9adac41818a840
Artut Fattakhov <86257431+ArchFa@users.noreply.github.com>
2022-07-12 15:54:28 +0500

    add

--- Untitled-1.py
+++ Untitled-1.py
@@ -138,7 +138,7 @@ st.plotly_chart(cx)
 
 cxx = px.bar(percentage_tasks_mobile, x='month', y='percentage_mobile',
             height=700,
-            width=500,
+            width=1200,
             title="Процент офферов через приложение",
             labels={'month':'Месяц', 'percentage_mobile':'Процент задач через приложение'},
             text_auto=True)
