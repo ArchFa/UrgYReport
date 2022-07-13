f5492e52042ce3690ee6b594eec904438c9fd983
Artut Fattakhov <86257431+ArchFa@users.noreply.github.com>
2022-07-13 15:47:56 +0500

    add

--- Untitled-1.py
+++ Untitled-1.py
@@ -15,67 +15,67 @@ st.title("Дашборд UrgY")
 ################################### основные вычисления ###################################
 
 # %%
-# count_task = st.file_uploader("Выбирете файл")
+count_task = st.file_uploader("Выбирете файл")
 
 
-# use_example_file = st.checkbox(
-#     "Использовать пример выгрузки", False, help="Будет использована базовая выгрузка, c 1 апреля по 1 июля"
-# )
+use_example_file = st.checkbox(
+    "Использовать пример выгрузки", False, help="Будет использована базовая выгрузка, c 1 апреля по 1 июля"
+)
 
-# # использование примера файла
-# if use_example_file:
-#     count_task = "offers_statuses_04_01_07_01.txt"
+# использование примера файла
+if use_example_file:
+    count_task = "offers_statuses_04_01_07_01.txt"
 
-# # использование загруженного файла
-# if count_task:
-#     df = pd.read_csv(count_task, sep='|')
-#     df = df.dropna()
+# использование загруженного файла
+if count_task:
+    df = pd.read_csv(count_task, sep='|')
+    df = df.dropna()
 
-#     df.columns = ['offer_id', 'offer_created_at','platform','count_responds', 'count_prematch']
+    df.columns = ['offer_id', 'offer_created_at','platform','count_responds', 'count_prematch']
 
-#     df['offer_created_at'] = pd.to_datetime(df['offer_created_at'])
-#     df.offer_created_at = df.offer_created_at.values.astype('M8[D]')
+    df['offer_created_at'] = pd.to_datetime(df['offer_created_at'])
+    df.offer_created_at = df.offer_created_at.values.astype('M8[D]')
 
-#     df['count_responds'] = df['count_responds'].astype(int)
-#     df['count_prematch'] = df['count_prematch'].astype(int)
+    df['count_responds'] = df['count_responds'].astype(int)
+    df['count_prematch'] = df['count_prematch'].astype(int)
 
-#     df['platform'] = df['platform'].str.strip()
-
-
-# with st.expander("Требования к загружаемому файлу"):
-#      st.info(
-#         """
-#              👆 Загрузите файл с расширением csv или txt. В файле должны стого содержаться следующие столбцы:
-#              - id оффера
-#              - дата создания оффера
-#              - платформа создания оффера
-#              - количество прематчей на задачу
-#              - количество матчей на задачу
-#              """
-#     )
+    df['platform'] = df['platform'].str.strip()
 
 
-# if not count_task or not use_example_file:
-#         st.stop()
+with st.expander("Требования к загружаемому файлу"):
+     st.info(
+        """
+             👆 Загрузите файл с расширением csv или txt. В файле должны стого содержаться следующие столбцы:
+             - id оффера
+             - дата создания оффера
+             - платформа создания оффера
+             - количество прематчей на задачу
+             - количество матчей на задачу
+             """
+    )
 
-# %%
-# df['month'] = df['offer_created_at'].dt.month
+
+if not count_task or not use_example_file:
+        st.stop()
 
 # %%
-df = pd.read_csv("/Users/arturfattahov/Downloads/Telegram Desktop/offers_statuses_04_01_07_01.txt", sep='|')
-
-df = df.dropna()
-
-df.columns = ['offer_id', 'offer_created_at','platform','count_responds', 'count_prematch']
-
-df['offer_created_at'] = pd.to_datetime(df['offer_created_at'])
-df.offer_created_at = df.offer_created_at.values.astype('M8[D]')
-
-df['count_responds'] = df['count_responds'].astype(int)
-df['count_prematch'] = df['count_prematch'].astype(int)
-
-df['platform'] = df['platform'].str.strip()
 df['month'] = df['offer_created_at'].dt.month
+
+# %%
+# df = pd.read_csv("/Users/arturfattahov/Downloads/Telegram Desktop/offers_statuses_04_01_07_01.txt", sep='|')
+
+# df = df.dropna()
+
+# df.columns = ['offer_id', 'offer_created_at','platform','count_responds', 'count_prematch']
+
+# df['offer_created_at'] = pd.to_datetime(df['offer_created_at'])
+# df.offer_created_at = df.offer_created_at.values.astype('M8[D]')
+
+# df['count_responds'] = df['count_responds'].astype(int)
+# df['count_prematch'] = df['count_prematch'].astype(int)
+
+# df['platform'] = df['platform'].str.strip()
+# df['month'] = df['offer_created_at'].dt.month
 
 # %%
 months = {
